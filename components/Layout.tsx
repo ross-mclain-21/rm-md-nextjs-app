bfbb37a0247b278269a6233244d90a5cc536a2f7
Ross McLain <ross.mclain21@gmail.com>
2022-04-28 13:48:58 -0400

    add google analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,6 +10,20 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>McLain Development</title>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
+        />
       </Head>
       <main ref={appRef} id="app" className="d-flex flex-column h-100 pt-5" data-testid="layout">
         <NavBar appRef={appRef} />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import React, { RefObject, useState } from 'react';
+import React, { RefObject, useEffect, useState } from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 import Layout from '../components/Layout';
@@ -8,6 +8,8 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import initFontAwesome from '../utils/initFontAwesome';
 import '../styles/globals.scss';
 import GlobalContext from '../components/common/GlobalContext';
+import * as ga from '../utils/googleAnalytics';
+import { useRouter } from 'next/router';
 
 initFontAwesome();
 
@@ -15,6 +17,22 @@ export default function App({ Component, pageProps }: AppProps) {
   const [homeRef, setHomeRef] = useState<RefObject<any>>(null);
   const [developRef, setDevelopRef] = useState<RefObject<any>>(null);
   const [hasScrolled, setHasScrolled] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <UserProvider>
