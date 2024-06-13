949abba79cc1a1e9928e76bbe4a45edff8032bb1
Ross McLain <ross.mclain@bluerabbit.vet>
2024-06-12 20:51:00 -0400

    [main] feat: update packages

--- components/home/ButWaitThereIsMore.tsx
+++ components/home/ButWaitThereIsMore.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import TypeWriter from '../common/TypeWriter';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { motion, useAnimation } from 'framer-motion';
@@ -14,14 +14,6 @@ const ButWaitThereIsMore = () => {
   let scrollToElement = () => {
     scrollTo(developRef);
   };
-  //
-  // useEffect(() => {
-  //   if (hasScrolled) {
-  //     controls.start('hidden');
-  //   } else {
-  //     controls.start('visible');
-  //   }
-  // }, [controls, hasScrolled]);
 
   return (
     <div className="flex-fill d-flex flex-column justify-content-end">
@@ -29,18 +21,21 @@ const ButWaitThereIsMore = () => {
         animate={controls}
         initial="visible"
         variants={serviceVariants}
-        className="d-flex flex-column align-items-center justify-content-center p-5">
+        className="d-flex flex-column align-items-center justify-content-center p-5"
+      >
         <TypeWriter message={'MORE ABOUT ME:'} classes="small code" typingSpeed={50} />
         <button
           className="btn btn-link"
           onClick={() => {
             scrollToElement();
-          }}>
+          }}
+        >
           <motion.h1
             transition={bounceTransition}
             animate={{
               y: ['5px', '-5px']
-            }}>
+            }}
+          >
             <FontAwesomeIcon icon={faChevronDown} />
           </motion.h1>
         </button>
