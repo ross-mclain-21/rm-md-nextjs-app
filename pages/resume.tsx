add0c6bec8bb5a790bf7ce3fbcbb41eb298c51c6
Ross McLain <ross.mclain@bluerabbit.vet>
2024-06-16 20:01:26 -0400

    [main] chore: remove custom promise.withResolvers

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -17,20 +17,6 @@ const Resume = () => {
 
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
-  //@ts-ignore
-  if (typeof Promise.withResolvers === 'undefined') {
-    if (window)
-      // @ts-expect-error This does not exist outside of polyfill which this is doing
-      window.Promise.withResolvers = function () {
-        let resolve, reject;
-        const promise = new Promise((res, rej) => {
-          resolve = res;
-          reject = rej;
-        });
-        return { promise, resolve, reject };
-      };
-  }
-
   const onDocumentLoadSuccess = (pdf: SetStateAction<any>) => {
     try {
       setNumPages(pdf._pdfInfo.numPages);
