81f90a5deccbca80b28141ee0086b47eb86b8e40
Ross McLain <mclain.development@gmail.com>
2024-10-04 17:12:24 -0400

    [main] fix: try and correct build issues

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -15,6 +15,19 @@ const Resume = () => {
 
   const resumePath = '/assets/files/resume.pdf';
 
+  if (typeof Promise.withResolvers === 'undefined') {
+    if (window)
+      // @ts-expect-error This does not exist outside of polyfill which this is doing
+      window.Promise.withResolvers = function () {
+        let resolve, reject;
+        const promise = new Promise((res, rej) => {
+          resolve = res;
+          reject = rej;
+        });
+        return { promise, resolve, reject };
+      };
+  }
+
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
   const onDocumentLoadSuccess = (pdf: SetStateAction<any>) => {
